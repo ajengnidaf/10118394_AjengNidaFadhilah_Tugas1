a007aeb2fab553ce1cc8baf7adc44a439897baa9
ajengnidaf <ajengnida99@gmail.com>
2020-04-02 13:14:57 +0700

    tugaske1

---
+++ src/provis9_praktikumm1/Provis9_praktikumm1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis9_praktikumm1;
+
+/**
+ *
+ * @author Profesional
+ */
+public class Provis9_praktikumm1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                Frame frm = new Frame();
+        frm.setVisible(true);
+    }
+}
+
